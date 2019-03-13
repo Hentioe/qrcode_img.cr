0c92df3e150163b6d24985d20692738b8fb7a584
Hentioe_Cl <me@bluerain.io>
2019-03-14 03:04:32 +0800

    Add zoom support

--- src/qrcode_img.cr
+++ src/qrcode_img.cr
@@ -4,23 +4,43 @@ require "stumpy_png"
 include StumpyPNG
 
 module QRcodeImg
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
   class QRcode
     setter input : String
+    setter meta_table : Array(Array(Bool))
+    setter size : Int32
 
-    def initialize(@input)
+    def initialize(@input, @zoom_level = 1)
+      @meta_table = Array(Array(Bool)).new
+      qr = QRencode::QRcode.new(@input, micro = true)
+      @size = qr.width * @zoom_level
+      qr.each_row do |row|
+        meta_row = Array(Bool).new
+        row.each do |byte|
+          if QRencode::Util.black? byte
+            @zoom_level.times do
+              meta_row << true
+            end
+          else
+            @zoom_level.times do
+              meta_row << false
+            end
+          end
+        end
+        @zoom_level.times do
+          @meta_table << meta_row
+        end
+      end
     end
 
     private def build_canvas
-      qr = QRencode::QRcode.new(@input)
-      canvas = Canvas.new(qr.width, qr.width)
+      canvas = Canvas.new(@size, @size)
 
       row_i, col_i = 0, 0
-      qr.each_row do |row|
-        # each row is `width` bytes
-        row.each do |byte|
-          if QRencode::Util.black? byte
+      @meta_table.each do |row|
+        row.each do |meta|
+          if meta
             color = RGBA.from_rgb_n(0, 0, 0, 8)
             canvas[row_i, col_i] = color
           else
